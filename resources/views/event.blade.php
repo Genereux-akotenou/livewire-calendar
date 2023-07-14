d4d0d1fa90ebfa04fd374850f7c064ab7971c492
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2023-07-14 15:29:11 +0100

    Update event.blade.php

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -2,9 +2,9 @@
     @if($eventClickEnabled)
         wire:click.stop="onEventClick('{{ $event['id']  }}')"
     @endif
-    class="bg-white rounded-lg border py-2 px-3 shadow-md cursor-pointer">
+    class="bg-white rounded-lg border py-2 px-3 shadow-sm cursor-pointer">
 
-    <p class="text-sm font-medium">
+    <p class="text-sm font-weight-bold">
         {{ $event['title'] }}
     </p>
     <p class="mt-2 text-xs">

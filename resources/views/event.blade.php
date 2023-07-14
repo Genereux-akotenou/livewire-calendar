c3fda073c355ca3d5253e4cbca18f034cb08bc85
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2023-07-14 16:07:39 +0100

    Update event.blade.php

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -1,10 +1,57 @@
+<style>
+    .bg-white {
+  background-color: #fff;
+}
+
+.rounded-lg {
+  border-radius: 0.5rem;
+}
+
+.border {
+  border-width: 1px;
+}
+
+.py-2 {
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+}
+
+.px-3 {
+  padding-left: 0.75rem;
+  padding-right: 0.75rem;
+}
+
+.shadow-md {
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+
+.cursor-pointer {
+  cursor: pointer;
+}
+
+.text-sm {
+  font-size: 0.875rem;
+}
+
+.font-medium {
+  font-weight: 500;
+}
+
+.mt-2 {
+  margin-top: 0.5rem;
+}
+
+.text-xs {
+  font-size: 0.75rem;
+}
+</style>
 <div
     @if($eventClickEnabled)
         wire:click.stop="onEventClick('{{ $event['id']  }}')"
     @endif
-    class="bg-white rounded-lg border py-2 px-3 shadow-sm cursor-pointer">
+    class="bg-white rounded-lg border py-2 px-3 shadow-md cursor-pointer">
 
-    <p class="text-sm font-weight-bold">
+    <p class="text-sm font-medium">
         {{ $event['title'] }}
     </p>
     <p class="mt-2 text-xs">

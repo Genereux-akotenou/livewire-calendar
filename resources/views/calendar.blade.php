94bfc199a3418320d718dea12c056c938b43cb49
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2023-07-14 16:05:11 +0100

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,26 +1,169 @@
-<div class="container">
-    @if($pollMillis !== null && $pollAction !== null)
-        <div wire:poll.{{ $pollMillis }}ms="{{ $pollAction }}"></div>
-    @elseif($pollMillis !== null)
-        <div wire:poll.{{ $pollMillis }}ms></div>
-    @endif
+<style>
+    /* Base class */
+.flex {
+  display: flex;
+}
 
+/* Responsive classes */
+@media (min-width: 640px) {
+  .sm\:flex {
+    display: flex;
+  }
+}
+
+@media (min-width: 768px) {
+  .md\:flex {
+    display: flex;
+  }
+}
+
+@media (min-width: 1024px) {
+  .lg\:flex {
+    display: flex;
+  }
+}
+
+/* Base class */
+.overflow-x-auto {
+  overflow-x: auto;
+}
+
+/* Responsive classes */
+@media (min-width: 640px) {
+  .sm\:overflow-x-auto {
+    overflow-x: auto;
+  }
+}
+
+@media (min-width: 768px) {
+  .md\:overflow-x-auto {
+    overflow-x: auto;
+  }
+}
+
+@media (min-width: 1024px) {
+  .lg\:overflow-x-auto {
+    overflow-x: auto;
+  }
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Responsive classes */
+@media (min-width: 640px) {
+  .sm\:w-full {
+    width: 100%;
+  }
+}
+
+@media (min-width: 768px) {
+  .md\:w-full {
+    width: 100%;
+  }
+}
+
+@media (min-width: 1024px) {
+  .lg\:w-full {
+    width: 100%;
+  }
+}
+
+/* Base class */
+.inline-block {
+  display: inline-block;
+}
+
+/* Base class */
+.min-w-full {
+  min-width: 100%;
+}
+
+/* Base class */
+.overflow-hidden {
+  overflow: hidden;
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Base class */
+.flex-row {
+  flex-direction: row;
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Base class */
+.flex-row {
+  flex-direction: row;
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Base class */
+.flex-row {
+  flex-direction: row;
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Base class */
+.flex-row {
+  flex-direction: row;
+}
+
+/* Base class */
+.w-full {
+  width: 100%;
+}
+
+/* Base class */
+.flex-row {
+  flex-direction: row;
+}
+</style>
+<div
+    @if($pollMillis !== null && $pollAction !== null)
+        wire:poll.{{ $pollMillis }}ms="{{ $pollAction }}"
+    @elseif($pollMillis !== null)
+        wire:poll.{{ $pollMillis }}ms
+    @endif
+>
     <div>
         @includeIf($beforeCalendarView)
     </div>
 
-    <div class="d-flex">
-        <div class="overflow-auto w-100">
-            <div class="d-inline-block min-w-100 overflow-hidden">
+    <div class="flex">
+        <div class="overflow-x-auto w-full">
+            <div class="inline-block min-w-full overflow-hidden">
 
-                <div class="w-100 d-flex flex-row">
+                <div class="w-full flex flex-row">
                     @foreach($monthGrid->first() as $day)
                         @include($dayOfWeekView, ['day' => $day])
                     @endforeach
                 </div>
 
                 @foreach($monthGrid as $week)
-                    <div class="w-100 d-flex flex-row">
+                    <div class="w-full flex flex-row">
                         @foreach($week as $day)
                             @include($dayView, [
                                     'componentId' => $componentId,
